089c39cee6761695d1c80ccb277fa7ae9d5a58d2
Omaury Marmolejo <omaury@tango.io>
2023-10-05 16:33:16 -0600

    Add Typescript solution for shape area

---
+++ typescript/shapeArea.ts
@@ -0,0 +1,8 @@
+// Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+
+// A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side.
+
+function solution(n: number): number {
+  const rimArea: number = (n * (n - 1)) / 2;
+  return rimArea * 4 + 1;
+}

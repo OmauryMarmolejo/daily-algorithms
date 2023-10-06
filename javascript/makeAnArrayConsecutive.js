a6e4976e1cce5f65a275e57009b2d6afd89bf858
Omaury Marmolejo <omaury@tango.io>
2023-10-06 15:17:15 -0600

    Add javascript solution for make an array consecutive

---
+++ javascript/makeAnArrayConsecutive.js
@@ -0,0 +1,23 @@
+// Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
+// Example
+
+// For statues = [6, 2, 3, 8], the output should be
+// solution(statues) = 3.
+
+// Ratiorg needs statues of sizes 4, 5 and 7.
+
+function solution(statues) {
+  if (statues.length === 1) {
+    return 0;
+  }
+
+  const sortedStatues = [...statues].sort((a, b) => a - b);
+  let requiredStatues = null;
+
+  for (i = 0; i < sortedStatues.length - 1; i++) {
+    requiredStatues += sortedStatues[i + 1] - sortedStatues[i] - 1;
+  }
+
+  return requiredStatues;
+}

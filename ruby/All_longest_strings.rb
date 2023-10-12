9bdc87f773fdbebe68d72060106eb7c7af30be5d
Omaury Marmolejo <omaury@tango.io>
2023-10-12 16:49:01 -0600

    Add Ruby solution for All Longest Strings

---
+++ ruby/All_longest_strings.rb
@@ -0,0 +1,11 @@
+# Given an array of strings, return another array containing all of its longest strings.
+
+# Example
+
+# For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+# solution(inputArray) = ["aba", "vcd", "aba"].
+
+def solution(inputArray)
+  max_length = inputArray.map {|a| a.length}.max
+  inputArray.select { |a| a.length == max_length}
+end

0479a242c40002ef8d2af23123ada9ea8516a05d
Omaury Marmolejo <omaury@tango.io>
2023-10-17 17:00:45 -0600

    Add Ruby solution for is lucky

---
+++ ruby/is_lucky.rb
@@ -0,0 +1,13 @@
+def solution(n)
+  array_number = n.to_s.split("")
+  middle_index = array_number.size / 2
+  
+  first_part = array_number.slice(0, middle_index)
+  second_part = array_number.slice(middle_index, array_number.size)
+
+  sum_array(first_part) == sum_array(second_part)
+end
+
+def sum_array(array)
+  array.map{|str| str.to_i }.reduce(:+)
+end

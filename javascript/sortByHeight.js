381022ded1a1770de2af8453a1f875b75dd168ec
Omaury Marmolejo <omaury@tango.io>
2023-10-18 16:25:27 -0600

    Add Javascript solution for Sort by Height

---
+++ javascript/sortByHeight.js
@@ -0,0 +1,16 @@
+// Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees. People can be very tall!
+
+// Example
+
+// For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
+// solution(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
+
+function solution(a) {
+  const sorted_heights = a
+    .filter((element) => element !== -1)
+    .sort((a, b) => a - b);
+
+  return a.map((element) => {
+    return element === -1 ? element : sorted_heights.shift();
+  });
+}
